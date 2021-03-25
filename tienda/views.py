d552ee93beb98b36b34e4eb3e19b9c1d199dacbf
Gus Rojas <gusm.rojas@gmail.com>
2021-03-24 20:30:20 -0600

    Iniciando proyecto

--- tienda/views.py
+++ tienda/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1> Mi negocio web </h1>")
+    return HttpResponse("<h1>Bienvenido a la tienda</h1>")
     
+
+
