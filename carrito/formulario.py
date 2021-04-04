29e7f7d7c4ad21152cedf9926f9f66c8ffbba6d2
Gus Rojas <gusm.rojas@gmail.com>
2021-04-04 15:02:50 -0500

    Carrito de compras implementado

---
+++ carrito/formulario.py
@@ -0,0 +1,12 @@
+from django import forms
+
+ELECCION_CANTIDAD_PRODCUTOS = [(i, str(i)) for i in range(1,21)]
+
+class FormularioAgregarProducto(forms.Form):
+    cantidad = forms.TypedChoiceField(choices=ELECCION_CANTIDAD_PRODCUTOS,
+                                        coerce=int)
+    sobreescribir = forms.BooleanField(required=False,
+                                        initial=False, 
+                                        widget=forms.HiddenInput)
+
+                                        
--- tienda/views.py
+++ tienda/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Categoria, Producto
 
 
+
 # Create your views here.
    
 def lista_productos(request, categoria_slug=None):
@@ -17,10 +18,11 @@ def lista_productos(request, categoria_slug=None):
                     'categorias': categorias,
                     'productos': productos})
 
-def producto_detalle(request, id, slug):
-    producto = get_object_or_404(Producto, id=id, 
-                                slug=slug, disponibilidad=True)
-                                
-    return render(request, 'tienda/productos/detalles.html', 
-                {'producto': producto,})
+from carrito.formulario import FormularioAgregarProducto
 
+def producto_detalle(request, id, slug): 
+    producto = get_object_or_404(Producto, id=id, slug=slug, disponibilidad=True)
+    form_carrito_producto = FormularioAgregarProducto()
+    return render(request, 'tienda/productos/detalles.html', 
+                    {'producto': producto,
+                     'form_carrito_producto': form_carrito_producto})
